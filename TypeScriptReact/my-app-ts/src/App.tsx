1e46f496b71db7d0c7e76ea71b8007ba85c6d657
andre.cabral <andre.cabral@db1.com.br>
2024-09-13 09:43:46 -0300

    ts banco

--- TypeScriptReact/my-app-ts/src/App.tsx
+++ TypeScriptReact/my-app-ts/src/App.tsx
@@ -1,13 +1,29 @@
 import './App.css';
 import Card from './components/Card';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <Card id={1}/>
-      <Card id={2}/>
-      <Card id={3}/>
-    </div>
+    <>
+      <Layout>
+        Hello World
+        <Card 
+          id={1}
+          details='TS'
+          paragraph='Front com TS'
+        />
+        <Card 
+          id={2}
+          details='HTML'
+          paragraph='Front com HTML'
+          />
+        <Card
+          id={3}
+          details='SQL'
+          paragraph='Query SQL'
+          />
+      </Layout>
+    </>
   );
 }
 
--- TypeScriptReact/my-app-ts/src/components/Card.tsx
+++ TypeScriptReact/my-app-ts/src/components/Card.tsx
@@ -1,10 +1,16 @@
 interface InterfaceCard {
-    id: number
+    id: number,
+    paragraph: string,
+    details: string
 }
 
-const Card = ({ id }: InterfaceCard) => {
+const Card = ({ id, paragraph, details }: InterfaceCard) => {
     return (
-        <h1>Card</h1>
+        <>
+        <h1>Card: {id}</h1>
+        <p>Paragrafo: {paragraph}</p>
+        <p>Detalhes: {details}</p>
+        </>
     );
 }
 
